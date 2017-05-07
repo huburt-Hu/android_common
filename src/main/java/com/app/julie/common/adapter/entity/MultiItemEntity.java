7b833fdf328f53af502b6c04f0b3c038d9bf6e6e
saber <654360340@qq.com>
2017-05-07 18:49:25 +0800

    add common adapter

---
+++ src/main/java/com/app/julie/common/adapter/entity/MultiItemEntity.java
@@ -0,0 +1,10 @@
+package com.app.julie.common.adapter.entity;
+
+/**
+ * https://github.com/CymChad/BaseRecyclerViewAdapterHelper
+ */
+public interface MultiItemEntity {
+
+    int getItemType();
+
+}
