6efd32c8f0742452879ff52aa1ffcb1f4296b586
saber <654360340@qq.com>
2017-05-07 10:58:48 +0800

    add base activity

---
+++ src/main/java/com/app/julie/common/base/BaseActivity.java
@@ -0,0 +1,19 @@
+package com.app.julie.common.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by julie
+ * <p>
+ * Created on 2017/5/5.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
